bd0d9e3c65f76c795294d404b13d0a893a2825c9
joynul24 <joynula919@gmail.com>
2025-01-12 19:04:38 +0600

    compete the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
 import Nav from './components/Nav/Nav'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
 
       <Nav></Nav>
+      <Banner></Banner>
      
     </>
   )
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,18 @@
 
 const Nav = () => {
     return (
-        <div className="my-12 container mx-auto flex justify-between items-center">
-            <h2 className="text-4xl font-bold">Recipe Calories</h2>
+        <div className="p-5 lg:p-0 mt-10 container mx-auto lg:flex  justify-between  items-center">
+            <div><h2 className="text-2xl lg:text-4xl font-bold text-center lg:text-left">Recipe Calories</h2></div>
             {/* nav items */}
-            <div className="flex items-center gap-12 opacity-70 cursor-pointer">
+            <div className="mt-5 justify-around lg:mt-0 flex items-center gap-12 opacity-70 cursor-pointer">
                 <p>Home</p>
                 <p>Recipes</p>
                 <p>About</p>
                 <p>Search</p>
             </div>
-            <h1 className="bg-[#0BE58A] p-3 rounded-full"><i className="text-4xl fa-solid fa-user"></i></h1>
+            <div className="flex justify-center mt-5 lg:mt-0">
+            <h1><i className="p-3 rounded-full bg-[#0BE58A]  text-4xl fa-solid fa-user"></i></h1>
+            </div>
         </div>
     );
 };

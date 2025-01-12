8de4132f17fb53adef6a481376c577db3afb29bb
joynul24 <joynula919@gmail.com>
2025-01-12 15:52:51 +0600

    compelete the navber

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,13 +4,13 @@ const Nav = () => {
         <div className="my-12 container mx-auto flex justify-between items-center">
             <h2 className="text-4xl font-bold">Recipe Calories</h2>
             {/* nav items */}
-            <div className="flex items-center gap-12">
+            <div className="flex items-center gap-12 opacity-70 cursor-pointer">
                 <p>Home</p>
                 <p>Recipes</p>
                 <p>About</p>
                 <p>Search</p>
             </div>
-            <h1><i className="text-4xl fa-solid fa-user"></i></h1>
+            <h1 className="bg-[#0BE58A] p-3 rounded-full"><i className="text-4xl fa-solid fa-user"></i></h1>
         </div>
     );
 };
